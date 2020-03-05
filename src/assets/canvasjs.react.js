157671c2b111024eab2cbb10e2a75ab6ed616b6f
rashzero <rashzero0@gmail.com>
2020-03-05 22:23:13 +0200

    edit canvas

--- src/assets/canvasjs.react.js
+++ src/assets/canvasjs.react.js
@@ -1,38 +1,37 @@
-var React = require('react');
-var CanvasJS = require('./canvasjs.min');
-CanvasJS = CanvasJS.Chart ? CanvasJS : window.CanvasJS;
+import React from 'react';
+import CanvasJS from './canvasjs.min';
 
 class CanvasJSChart extends React.Component {
-	static _cjsContainerId = 0
-	constructor(props) {		
-		super(props);		
-		this.options = props.options ? props.options : {};		
-		this.containerProps = props.containerProps ? props.containerProps : {marginTop: "25px", marginLeft: "10%", width: "80%", position: "relative", };
-		this.containerProps.height = props.containerProps && props.containerProps.height ? props.containerProps.height : this.options.height ? this.options.height + "px" : "400px";
-		this.chartContainerId = "canvasjs-react-chart-container-" + CanvasJSChart._cjsContainerId++;
-	}	
-	componentDidMount() {	
-		this.chart = new CanvasJS.Chart(this.chartContainerId, this.options);
-		this.chart.render();
-		
-		if(this.props.onRef)
-			this.props.onRef(this.chart);
-	}	
+  static _cjsContainerId = 0
+  constructor(props) {		
+    super(props);		
+    this.options = props.options ? props.options : {};		
+    this.containerProps = props.containerProps ? props.containerProps : {marginTop: "25px", marginLeft: "10%", width: "80%", position: "relative", };
+    this.containerProps.height = props.containerProps && props.containerProps.height ? props.containerProps.height : this.options.height ? this.options.height + "px" : "400px";
+    this.chartContainerId = "canvasjs-react-chart-container-" + CanvasJSChart._cjsContainerId++;
+  }	
+  componentDidMount() {	
+    this.chart = new CanvasJS.Chart(this.chartContainerId, this.options);
+    this.chart.render();
+    
+    if(this.props.onRef)
+      this.props.onRef(this.chart);
+  }	
     shouldComponentUpdate(nextProps, nextState){
         return !(nextProps.options === this.options);
     }
-	componentDidUpdate() {
-		this.chart.options = this.props.options;
-		this.chart.render();
-	}
-	componentWillUnmount() {
-		this.chart.destroy();
-		if(this.props.onRef)
-			this.props.onRef(undefined);
-	}		
-	render() {				
-		return <div id={this.chartContainerId} style={this.containerProps}/>		
-	}	
+  componentDidUpdate() {
+    this.chart.options = this.props.options;
+    this.chart.render();
+  }
+  componentWillUnmount() {
+    this.chart.destroy();
+    if(this.props.onRef)
+      this.props.onRef(undefined);
+  }		
+  render() {				
+    return <div id={this.chartContainerId} style={this.containerProps}/>		
+  }	
 }
 
 var CanvasJSReact = {
